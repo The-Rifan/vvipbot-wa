050a53e254c88c770ce29c0ac4664c8b50900cc1
The-Rifan <78841725+The-Rifan@users.noreply.github.com>
2021-02-23 08:48:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'THE_RIFAN BOT'; 
+const instagram = 'The_Rifan'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:RIFAN\n' // Nama kamu
+            + 'ORG:THE_RIFAN;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6289652898494:+62 896-5289-8494\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
@@ -1446,7 +1446,7 @@ Silakan pilih :
 *.Truth*
 *.Dare*
 
-*Selesaikan perintah untuk melakukan TOD selanjutnya* ⚠️` ,MessageType.text, {quoted: m});
+*Selesaikan perintah untuk melakukan selanjutnya* ⚠️` ,MessageType.text, {quoted: m});
 
 
 }
